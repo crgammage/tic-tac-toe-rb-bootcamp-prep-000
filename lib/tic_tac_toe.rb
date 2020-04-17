8a3727c7229db952281165744dc64789b9313940
masterful-array-3850 <crgammage@gmail.com>
2020-04-17 15:36:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,7 +36,7 @@ end
 
 def valid_move?(board, index)
   spot = index.to_i
-  if position_taken?(board, spot) == false && spot.between.(0, 8)
+  if position_taken?(board, spot) == false && spot.between?(0, 8)
     return true
   else
     return false
